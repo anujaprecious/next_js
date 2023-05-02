ee03fd17315aa0856909070ad50e39fc5fa991e1
anujaprecious <anuja.patel@preciousinfosystem.com>
2023-05-02 18:57:45 +0530

    Third commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,25 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [val,setVal] = useState("Anuja");
+  const [print,setPrint]=useState(false);
 
+const handleSubmit=(e)=>{
+  setVal(e.target.value)
+ setPrint(false)
+}
 
   return (
     <main>
 
       <h1>Hey! Anuja Patel</h1>
-      <form>
-    <input type='text' value={val} onChange={(e)=>setVal(e.target.value)} />
-    </form>
 
+    <input type='text'  onChange={handleSubmit}/>
 
-    <h1>{val}</h1>
+     <button onClick={()=>setPrint(true)}>submit</button>
+     {
+      print?<h1>{val}</h1>:null
+    }
+
     </main>
   )
 }
