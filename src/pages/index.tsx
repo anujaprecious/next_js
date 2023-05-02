e4394bd4f55858c23cdeecb9918ec9f0ef50bdf4
anujaprecious <anuja.patel@preciousinfosystem.com>
2023-05-02 18:20:57 +0530

    Second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import {useState} from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <h1 className={`${styles.main} ${inter.className}`}>Hey! Anuja Patel</h1>
+  const [val,setVal] = useState("Anuja");
 
-    </>
+
+  return (
+    <main>
+
+      <h1>Hey! Anuja Patel</h1>
+      <form>
+    <input type='text' value={val} onChange={(e)=>setVal(e.target.value)} />
+    </form>
+
+
+    <h1>{val}</h1>
+    </main>
   )
 }
